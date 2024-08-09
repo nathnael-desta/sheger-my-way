5fb30ce068180cb7fefc5994def63ddfa8c601a1
Nathnael Desta <destanathnaelataro@gmail.com>
2024-08-09 10:18:34 +0300

    finish routes

--- sheger-my-way-app/src/components/Buses.vue
+++ sheger-my-way-app/src/components/Buses.vue
@@ -86,17 +86,228 @@
             <div class="route-list-container">
                 <div class="route-list">
                     <div
-                        v-for="(line, index) in routeListCreator(bus.route).order"
-                        :class="{ normal: line.type === 'normal', curved: line.type === 'curved', normalEnding: line.type === 'normalEnding', curvedEnding: 'curvedEnding'}"
+                        v-for="(line, index) in routeListCreator(bus.route)
+                            .order"
+                        :class="{
+                            normal: line.type === 'normal',
+                            curved: line.type === 'curved',
+                            normalEnding: line.type === 'normalEnding',
+                            curvedEnding: line.type === 'curvedEnding',
+                        }"
                     >
-                        <div v-if="line.type === 'normal' " class="">{{ location }}</div>
+                        <template
+                            v-for="(location, index) in line.data"
+                            v-if="line.type === 'normal'"
+                        >
+                            <div class="">
+                                {{ location }}
+                            </div>
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                v-if="index !== 2"
+                            >
+                                <path
+                                    d="M15 6.66663L18.3333 9.99996L15 13.3333"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                <path
+                                    d="M1.66675 10H18.3334"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                        </template>
+
+                        <template v-if="line.type === 'curved'">
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="bottomCurved"
+                                v-if="index !== routeListCreator(bus.route).order.length - 1"
+                            >
+                                <path
+                                    d="M11.6666 12.5L7.49992 16.6667L3.33325 12.5"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                <path
+                                    d="M16.6667 3.33337H10.8333C9.94928 3.33337 9.10143 3.68456 8.47631 4.30968C7.85119 4.93481 7.5 5.78265 7.5 6.66671V16.6667"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+
+                            <div class="">
+                                {{ line.data[0] }}
+                            </div>
+
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M5 13.3333L1.66667 9.99998L5 6.66665"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                <path
+                                    d="M18.3333 10L1.66659 10"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+
+                            <div class="">
+                                {{ line.data[1] }}
+                            </div>
+
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="leftCurved"
+                                
+                            >
+                                <path
+                                    d="M7.50016 8.33337L3.3335 12.5L7.50016 16.6667"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                <path
+                                    d="M16.6668 3.33337V9.16671C16.6668 10.0508 16.3156 10.8986 15.6905 11.5237C15.0654 12.1489 14.2176 12.5 13.3335 12.5H3.3335"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                        </template>
+
+                        <template
+                            v-if="line.type === 'normalEnding'"
+                            v-for="(location, index) in line.data"
+                        >
+                            <div class="">{{ location }}</div>
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                v-if="index !== line.data.length - 1"
+                            >
+                                <path
+                                    d="M15 6.66663L18.3333 9.99996L15 13.3333"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                <path
+                                    d="M1.66675 10H18.3334"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                        </template>
+
+                        <template v-if="line.type === 'curvedEnding'">
+                            <div class="">{{ line.data[0] }}</div>
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="leftCurved"
+                            >
+                                <path
+                                    d="M7.50016 8.33337L3.3335 12.5L7.50016 16.6667"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                <path
+                                    d="M16.6668 3.33337V9.16671C16.6668 10.0508 16.3156 10.8986 15.6905 11.5237C15.0654 12.1489 14.2176 12.5 13.3335 12.5H3.3335"
+                                    stroke="#416084"
+                                    stroke-width="1.25"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                        </template>
+
+                        <!-- <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="leftCurved"
+                            v-if="index === 1 && line.type === 'normal'"
+                        >
+                            <path
+                                d="M7.50016 8.33337L3.3335 12.5L7.50016 16.6667"
+                                stroke="#416084"
+                                stroke-width="1.25"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M16.6668 3.33337V9.16671C16.6668 10.0508 16.3156 10.8986 15.6905 11.5237C15.0654 12.1489 14.2176 12.5 13.3335 12.5H3.3335"
+                                stroke="#416084"
+                                stroke-width="1.25"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+
+                        <div
+                            class=""
+                            v-if="line.type === 'normal'"
+                        >
+                            {{ location }}
+                        </div>
                         <svg
                             width="20"
                             height="20"
                             viewBox="0 0 20 20"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
-                            v-if="index !== 2"
+                            v-if="
+                                index !== line.data.length - 1 &&
+                                line.type === 'normal'
+                            "
                         >
                             <path
                                 d="M15 6.66663L18.3333 9.99996L15 13.3333"
@@ -113,8 +324,39 @@
                                 stroke-linejoin="round"
                             />
                         </svg>
-                    </div>
-                    <div
+
+                        <div
+                            class=""
+                            v-if="line.type === 'normal'"
+                        >
+                            {{ location }}
+                        </div>
+                        <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="leftCurved"
+                            v-if="line.type === 'normal'"
+                        >
+                            <path
+                                d="M7.50016 8.33337L3.3335 12.5L7.50016 16.6667"
+                                stroke="#416084"
+                                stroke-width="1.25"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M16.6668 3.33337V9.16671C16.6668 10.0508 16.3156 10.8986 15.6905 11.5237C15.0654 12.1489 14.2176 12.5 13.3335 12.5H3.3335"
+                                stroke="#416084"
+                                stroke-width="1.25"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    </div> -->
+                        <!-- <div
                         class="curved"
                         v-if="line.type === 'curved'"
                         v-for="(location, index) in line.data"
@@ -254,12 +496,12 @@
                                 stroke-linejoin="round"
                             />
                         </svg>
+                    </div> -->
                     </div>
                 </div>
             </div>
-        </div>
 
-        <!-- <div
+            <!-- <div
             class="route"
             v-if="subBus.active === true && subBus.index === index"
         >
@@ -453,12 +695,13 @@
                 </div>
             </div>
         </div> -->
-        <div
-            class="buttons"
-            v-if="subBus.active === true && subBus.index === index"
-        >
-            <div class="cancel">Cancel</div>
-            <div class="track">Track</div>
+            <div
+                class="buttons"
+                v-if="subBus.active === true && subBus.index === index"
+            >
+                <div class="cancel">Cancel</div>
+                <div class="track">Track</div>
+            </div>
         </div>
     </div>
 </template>
